c99b61261a376eb6aba4764bcea0dd7f4447c6bd
Andreabafa1444 <116229674+Andreabafa1444@users.noreply.github.com>
2026-03-03 21:26:35 -0600

    parcial04 Add prevent unsave changes guard

--- client/src/core/guards/auth-guard.ts
+++ client/src/core/guards/auth-guard.ts
@@ -1,16 +1,18 @@
 import { CanActivateFn } from '@angular/router';
 import { AccountService } from '../services/account-service';
-import { inject } from '@angular/core';
+import { inject } from '@angular/core/primitives/di';
 import { ToastService } from '../services/toast-service';
 
 export const authGuard: CanActivateFn = () => {
   const accountService = inject(AccountService);
   const toast = inject(ToastService);
-
-  if (accountService.currentUser()) {
+  if(accountService.currentUser()) {
     return true;
-  } else {
-    toast.error("You shall not pass!")
+  }
+  else{
+    toast.error("You must be logged in to access this page");
     return false;
   }
+
+  
 };
