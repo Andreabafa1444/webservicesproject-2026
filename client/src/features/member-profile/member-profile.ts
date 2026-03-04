0fff503af04ebecc1766f0e94d7338fe7352847f
Andreabafa1444 <116229674+Andreabafa1444@users.noreply.github.com>
2026-03-03 21:59:30 -0600

    Parcial 04 Enhance edit form

--- client/src/features/member-profile/member-profile.ts
+++ client/src/features/member-profile/member-profile.ts
@@ -5,40 +5,40 @@ import { DatePipe } from '@angular/common';
 import { MembersService } from '../../core/services/members-service';
 import { FormsModule, NgForm } from '@angular/forms';
 import { ToastService } from '../../core/services/toast-service';
+import { AccountService } from '../../core/services/account-service';
+
 @Component({
   selector: 'app-member-profile',
-  imports: [DatePipe,FormsModule],
+  imports: [DatePipe, FormsModule],
   templateUrl: './member-profile.html',
   styleUrl: './member-profile.css'
 })
 export class MemberProfile implements OnInit, OnDestroy{
   
   @ViewChild('MemberProfileEditForm') memberProfileEditForm?: NgForm;
-    @HostListener('window:beforeunload', ['$event']) notify ($event: BeforeUnloadEvent) {
+  @HostListener('window:beforeunload', ['$event']) notify ($event: BeforeUnloadEvent) {
     if(this.memberProfileEditForm?.dirty) {
       $event.preventDefault();
     }
   }
-  private route = inject(ActivatedRoute);
-    private toast= inject(ToastService);
-  protected member = signal<Member | undefined>(undefined);
-    protected membersService = inject(MembersService);
- protected editablemember: Editablemember = {
+  private accountService = inject(AccountService);
+  private toast= inject(ToastService);
+ 
+  protected membersService = inject(MembersService);
+  protected editablemember: Editablemember = {
     displayName:  '',
       description:  '',
       city: '',
       country:  ''
   }
-   ngOnInit() {
-    this.route.parent?.data.subscribe(data => {
-      this.member.set(data['member']);
-    });
 
+  ngOnInit() {
+    
     this.editablemember = {
-      displayName: this.member()?.displayName || '',
-      description: this.member()?.description || '',
-      city: this.member()?.city || '',
-      country: this.member()?.country || ''
+      displayName: this.membersService.member()?.displayName || '',
+      description: this.membersService.member()?.description || '',
+      city: this.membersService.member()?.city || '',
+      country: this.membersService.member()?.country || ''
     };
     
   }
@@ -50,14 +50,23 @@ export class MemberProfile implements OnInit, OnDestroy{
   }
 
   updateProfile() {
-    if (!this.member()) return;
-    const updatedMember = {...this.member(), ...this.editablemember};
+    if (!this.membersService.member()) return;
+    const updatedMember = {...this.membersService.member(), ...this.editablemember};
     this.membersService.updateMember(this.editablemember).subscribe({
       next: () => {
-        this.toast.success('Profile updated successfully');
+        const currentUser = this.accountService.currentUser();
+        if (currentUser && updatedMember.displayName !== currentUser?.displayName) {
+          currentUser.displayName = updatedMember.displayName;
+          this.accountService.setCurrentUser(currentUser);
+        }
+        
         this.membersService.editMode.set(false);
+        this.membersService.member.set(updatedMember as Member);
         this.memberProfileEditForm?.reset(updatedMember);
+        this.toast.success('Profile updated successfully');
       }
     });
   }
+
+  
 }
